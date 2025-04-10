52e0bf9ce9e9184d49d201080f93c8d4d50ca24f
KirillDripp <k.poita@ukma.edu.ua>
2025-04-10 20:14:35 +0300

    Remove build artifacts from repo

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Admin/Desktop/Task6
-# Build directory: C:/Users/Admin/Desktop/Task6/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(bst_test "C:/Users/Admin/Desktop/Task6/build/bst_test.exe")
-set_tests_properties(bst_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Admin/Desktop/Task6/CMakeLists.txt;30;add_test;C:/Users/Admin/Desktop/Task6/CMakeLists.txt;0;")
